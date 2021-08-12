d2362c2acf3f965634c931fbde758fd57e6b3787
Dakouri M. Kobri <maurille.kobri@gmail.com>
2021-08-12 08:33:14 +0200

    1. Write Item component's properties 2. Add subsequent properties to PendingItemList 3. Add DUMMY_DATA to ListPage component

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,12 @@
-
-export default function Item() {
-    return (
-        <div>
-            
-            <li><input type="checkbox" />item 1</li>
-            <li><input type="checkbox" />item 2</li>
-            
-        </div>
-    )
+export default function Item(props) {
+  return (
+    <div>
+      <li key={props.id}>
+        <input type="checkbox" acquired={props.acquired} />
+        <label>{props.name}</label>
+        <label> {props.price}:-</label>
+        <img src={props.pictureURL} alt={props.name} />
+      </li>
+    </div>
+  );
 }
--- src/components/PendingItemList.jsx
+++ src/components/PendingItemList.jsx
@@ -1,11 +1,20 @@
 import Item from "./Item";
 
-
-export default function PendingItemList() {
-    return (
-        <div>
-           <h3>Pending items</h3>
-           <Item />
-        </div>
-    )
+export default function PendingItemList(props) {
+  return (
+    <div>
+      <h3>Pending items</h3>
+      <ul>
+        {props.items.map((item) => (
+          <Item
+            key={item.id}
+            name={item.name}
+            price={item.price}
+            acquired={item.aquired}
+            pictureURL={item.pictureURL}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
