2f7bc6468de0033c3b809d5de34e23224b63cabb
Dakouri M. Kobri <maurille.kobri@gmail.com>
2021-08-12 11:30:48 +0200

    Add handleCheck funtion for Item aquire status change in Item component

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 export default function Item(props) {
+  const [acquired, setAcquired] = useState(props.acquired);
+
+  function handleCheck() {
+    setAcquired(!acquired);
+  }
+
   return (
     <div>
       <li key={props.id}>
-        <input type="checkbox" acquired={props.acquired} />
+        <input
+          type="checkbox"
+          id={props.id}
+          onChange={handleCheck}
+          acquired={props.acquired}
+          value={props.acquired}
+        />
         <label>{props.name}</label>
         <label> {props.price}:-</label>
-        <img src={props.pictureURL} alt={props.name} />
+        {/* <img src={props.pictureURL} alt={props.name} /> */}
       </li>
     </div>
   );
