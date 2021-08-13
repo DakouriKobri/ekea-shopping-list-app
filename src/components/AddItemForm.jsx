18eaf1e7bb0374d57b5b3500dc824b63ef3bdc5c
Dakouri M. Kobri <maurille.kobri@gmail.com>
2021-08-13 13:46:31 +0200

    Add useRef hook to AddItemForm component to handle input from user

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,18 +1,40 @@
+import { useRef } from "react";
+
 export default function AddItemForm() {
+  const nameInputRef = useRef();
+  const priceInputRef = useRef();
+  const pictureInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredPrice = priceInputRef.current.value;
+    const enteredPicture = pictureInputRef.current.value;
+
+    const itemData = {
+      name: enteredName,
+      price: enteredPrice,
+      picture: enteredPicture,
+    };
+
+    console.log(itemData);
+  }
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={submitHandler}>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" required id="name" />
+          <input type="text" required id="name" ref={nameInputRef} />
         </div>
         <div>
           <label htmlFor="price">Price</label>
-          <input type="text" required id="price" />
+          <input type="text" required id="price" ref={priceInputRef} />
         </div>
         <div>
           <label htmlFor="picture">Item picture</label>
-          <input type="url" required id="picture" />
+          <input type="url" required id="picture" ref={pictureInputRef} />
         </div>
         <div>
           <button>Add Item</button>
